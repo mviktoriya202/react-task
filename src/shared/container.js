986327ee339385fbb587c8257ffdaec7b03c558d
Viktoriya <m.viktoriya202@gmail.com>
2023-05-08 13:14:11 +0400

    react task

---
+++ src/shared/container.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const ContainerSC = styled.div`
+  max-width: 1503px;
+  width: 100%;
+  height: auto;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 80px;
+
+  @media screen and (max-width: 1200px) {
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+`;
